fb71e775b57327b5384f2187cd5d2076a5a08442
VitaliSantalau <v.santalau@gmail.com>
2021-07-20 23:19:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array) return 0;
+  return array.reduce((res, elem) => {
+    if(elem < res) res = elem;
+    return res;
+  }, 0)
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array) return 0;
+  let res = 0;
+  for(let i = 0; i < array.length; i++ ) {
+    array[i] > res ? (res = array[i]) : res;
+  }
+  return res;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0) return 0;
+  return array.reduce((sum, elem) => {
+    return sum+=elem;
+  }, 0) / array.length;
 }
